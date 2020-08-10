a827546c7baafdc400c10d4be050aad2d6ffd928
Alok Rahul <alok.rahul.devops@gmail.com>
2020-08-10 22:25:07 +0530

    Initial commit for first day

---
+++ Excercises/DataTypes/programsOfInterest.py
@@ -0,0 +1,21 @@
+##Just a Random Code
+# name = 'Alok Rahul'
+# age = 90
+# relationship_status = 'Single'
+# relationship_status = 'It\'s complicated'
+# print(relationship_status)
+
+
+# #Program to calculate my age as per  the birth year
+# birth_year = int(input('What year were you born ? '))
+# age = 2020 - birth_year
+# print(f'your age is : {age}')
+
+
+# #Program to check the password Checker
+# username = input('Enter your Username: ')
+# password = input('Please enter your password: ')
+# password_length = len(password)
+# hidden_password = '*' * password_length
+
+# print(f'{username} , your password, {hidden_password}, is {password_length} letters long')
